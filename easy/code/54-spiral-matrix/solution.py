b90a1d35752c6353ba0a022ae2246e17129699cd
igor-souza <igorrs.rib@gmail.com>
2024-02-25 00:51:30 -0300

    removing unecessary variable

--- easy/code/54-spiral-matrix/solution.py
+++ easy/code/54-spiral-matrix/solution.py
@@ -4,27 +4,26 @@ class Solution:
         n = len(matrix[0])
         
         if (resultAux):
-            result = resultAux
-            result.extend(matrix[0][::])
+            resultAux.extend(matrix[0][::])
         else:    
-            result = matrix[0][::]
+            resultAux = matrix[0][::]
         
         if (m == 1 or n == 1):
             for i in range(1,m):    
-                result.append(matrix[i][n-1])
-            return result
+                resultAux.append(matrix[i][n-1])
+            return resultAux
         
         for i in range(1,m):    
-            result.append(matrix[i][n-1])
+            resultAux.append(matrix[i][n-1])
             
         for j in range(n-2,-1,-1):
-            result.append(matrix[m-1][j])
+            resultAux.append(matrix[m-1][j])
             
         for k in range(m-2,0,-1):
-            result.append(matrix[k][0])
+            resultAux.append(matrix[k][0])
         
 
         if (m ==2 or n == 2):
-            return result
+            return resultAux
         else:
-            return self.spiralOrder([row[1:-1] for row in matrix[1:m-1]],result)
+            return self.spiralOrder([row[1:-1] for row in matrix[1:m-1]],resultAux)
